a40b1279f548384ebdd96b46eb7c0bc3c7af2e14
Thomas Payer <payer@raum42.at>
2013-09-12 17:53:55 +0200

    added internal cache

--- config/module.config.php
+++ config/module.config.php
@@ -7,6 +7,19 @@ return array(
             'db_master' => 'Core42\Db\Adapter\MasterServiceFactory',
             'db_slave' => 'Core42\Db\Adapter\SlaveServiceFactory',
             'translator' => 'Zend\I18n\Translator\TranslatorServiceFactory',
+            'cache_intern' => 'Zend\Cache\Service\StorageCacheFactory',
+        ),
+    ),
+    
+    'cache' => array(
+        'adapter' => array(
+    	   'name' => 'filesystem',
+            'options' => array(
+	           'cache_dir' => 'data/cache/'
+            ),
+        ),
+        'plugins' => array(
+        	'Serializer'
         ),
     ),
 
