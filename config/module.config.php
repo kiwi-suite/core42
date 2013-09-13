d2e422275fbf5a637b2ea0c9e34d93a8f1467e3b
Thomas Payer <payer@raum42.at>
2013-09-13 08:20:50 +0200

    cache is using abstract factory now

--- config/module.config.php
+++ config/module.config.php
@@ -6,20 +6,24 @@ return array(
         'factories' => array(
             'db_master' => 'Core42\Db\Adapter\MasterServiceFactory',
             'db_slave' => 'Core42\Db\Adapter\SlaveServiceFactory',
-            'translator' => 'Zend\I18n\Translator\TranslatorServiceFactory',
-            'cache_intern' => 'Zend\Cache\Service\StorageCacheFactory',
+        ),
+        'abstract_factories' => array(
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
         ),
     ),
     
-    'cache' => array(
-        'adapter' => array(
-    	   'name' => 'filesystem',
-            'options' => array(
-	           'cache_dir' => 'data/cache/'
+    'caches' => array(
+        'Cache\Intern' => array(
+            'adapter' => array(
+        	   'name' => 'filesystem',
+                'options' => array(
+                    'cache_dir' => 'data/cache/',
+                    'namespace' => 'cache_intern'
+                ),
             ),
-        ),
-        'plugins' => array(
-        	'Serializer'
+            'plugins' => array(
+                'Serializer'
+            ),
         ),
     ),
 
