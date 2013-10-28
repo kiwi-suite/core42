ea6157f3e5a4aae0ca41aaa818266c5243f761e1
Bernhard Efler <efler@raum42.at>
2013-10-28 23:04:19 +0100

    JsonViewModel erweitert

---
+++ src/Core42/View/Model/JsonModel.php
@@ -0,0 +1,40 @@
+<?php
+namespace Core42\View\Model;
+
+use Zend\View\Model\JsonModel as ZendJsonModel;
+
+class JsonModel extends ZendJsonModel
+{
+    /**
+     * The encoded JSON string
+     *
+     * @var string
+     */
+    protected $jsonString = null;
+
+    /**
+     * Set the already encoded JSON string
+     *
+     * @param  string $json
+     * @return JsonModel
+     */
+    public function setJsonString($json)
+    {
+        $this->jsonString = $json;
+        return $this;
+    }
+
+    /**
+     * Serialize to JSON
+     *
+     * @return string
+     */
+    public function serialize()
+    {
+        //todo: eventuell mergen
+        if ($this->jsonString !== null) {
+            return $this->jsonString;
+        }
+        return parent::serialize();
+    }
+}
