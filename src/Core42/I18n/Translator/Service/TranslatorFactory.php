bd82793727ec9d7a2cb8bc92443ad8e9f17b0150
luchaos <lcs@luchaos.at>
2015-05-04 17:57:24 +0200

    handle missing translations through service action as optionally defined in config

--- src/Core42/I18n/Translator/Service/TranslatorFactory.php
+++ src/Core42/I18n/Translator/Service/TranslatorFactory.php
@@ -9,6 +9,7 @@
 
 namespace Core42\I18n\Translator\Service;
 
+use Zend\I18n\Translator\Translator;
 use Zend\Mvc\Service\TranslatorServiceFactory;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
@@ -25,6 +26,16 @@ class TranslatorFactory extends TranslatorServiceFactory
         $translator->getTranslator()->setPluginManager(
             $serviceLocator->get('MvcTranslatorPluginManager')
         );
+
+        if ($translator->isEventManagerEnabled()) {
+            $config = $serviceLocator->get('Config');
+            if(isset($config['translator']['missing_translations_handler'])) {
+                $missingConfig = $config['translator']['missing_translations_handler'];
+                if(isset($missingConfig['service']) && isset($missingConfig['action'])) {
+                    $translator->getEventManager()->attach(Translator::EVENT_MISSING_TRANSLATION, [$serviceLocator->get($missingConfig['service']), $missingConfig['action']]);
+                }
+            }
+        }
 
         return $translator;
     }
