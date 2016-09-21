92bc9c68a947f447729df2ba19d3bf079bcab629
Thomas Payer <payer@raum42.at>
2016-09-21 13:06:00 +0200

    modifications for new kiwi sceleton layout

---
+++ bootstrap/bootstrap.php
@@ -0,0 +1,53 @@
+<?php
+use Zend\Mvc\Application;
+use Zend\Stdlib\ArrayUtils;
+use Core42\Error\ErrorHandler;
+
+require 'config/settings.config.php';
+
+if (file_exists('data/maintenance/on')) {
+    http_response_code(503);
+
+    ob_start();
+    include_once 'data/maintenance/template.phtml';
+    echo ob_get_clean();
+    return;
+}
+
+if (php_sapi_name() === 'cli-server') {
+    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    if (__FILE__ !== $path && is_file($path)) {
+        return false;
+    }
+    unset($path);
+}
+
+require 'vendor/autoload.php';
+require 'config/environment.config.php';
+
+try {
+    ErrorHandler::setErrorTemplate('data/error/template.phtml');
+
+    if (! class_exists(Application::class)) {
+        throw new RuntimeException(
+            "Unable to load application.\n"
+            . "- Type `composer install` if you are developing locally.\n"
+            . "- Type `vagrant ssh -c 'composer install'` if you are using Vagrant.\n"
+            . "- Type `docker-compose run zf composer install` if you are using Docker.\n"
+        );
+    }
+    ErrorHandler::registerShutdown();
+
+    $appConfig = require 'config/application.config.php';
+    $developmentConfig = require 'config/development.config.php';
+
+    if (!empty($developmentConfig)) {
+        $appConfig = ArrayUtils::merge($appConfig, $developmentConfig);
+    }
+
+    Application::init($appConfig)->run();
+} catch (\Throwable $e) {
+    echo ErrorHandler::init($e);
+} catch (\Exception $e) {
+    echo ErrorHandler::init($e);
+}
