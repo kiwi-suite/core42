4ee473b8ec30e62d3eaa95bc4d894cb09c3647b5
Thomas Payer <payer@raum42.at>
2016-10-15 07:48:21 +0200

    fix: wrong config name

--- config/assets.config.php
+++ config/assets.config.php
@@ -4,7 +4,7 @@ namespace Core42;
 return [
     'assets' => [
         'asset_url'         => null,
-        'asset_base_path'   => null,
+        'asset_path'        => null,
         'prepend_commit'    => false,
         'prepend_base_path' => true,
         'directories'       => [],
