cf63a99953f8dfe223e63c064e62823f944bd320
Thomas Payer <payer@raum42.at>
2016-12-19 22:58:10 +0100

    added logger plugin manager

---
+++ src/Log/Service/LoggerPluginManagerFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * core42
+ *
+ * @package core42
+ * @link https://github.com/raum42/core42
+ * @copyright Copyright (c) 2010 - 2016 raum42 (https://www.raum42.at)
+ * @license MIT License
+ * @author raum42 <kiwi@raum42.at>
+ */
+
+namespace Core42\Log\Service;
+
+use Core42\Log\Service\AbstractLoggerFactory;
+use Core42\Log\Service\LoggerPluginManager;
+use Interop\Container\ContainerInterface;
+use Interop\Container\Exception\ContainerException;
+use Zend\ServiceManager\Exception\ServiceNotCreatedException;
+use Zend\ServiceManager\Exception\ServiceNotFoundException;
+use Zend\ServiceManager\Factory\FactoryInterface;
+
+class LoggerPluginManagerFactory implements FactoryInterface
+{
+    /**
+     * Create an object
+     *
+     * @param  ContainerInterface $container
+     * @param  string $requestedName
+     * @param  null|array $options
+     * @return object
+     * @throws ServiceNotFoundException if unable to resolve the service.
+     * @throws ServiceNotCreatedException if an exception is raised when
+     *     creating a service.
+     * @throws ContainerException if any other error occurs
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $plugin = new LoggerPluginManager($container);
+        $plugin->addAbstractFactory(new AbstractLoggerFactory());
+
+        return $plugin;
+    }
+}
