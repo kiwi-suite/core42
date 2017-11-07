6010e3ef711c44aa26f4c66ba3fe95c51e6edda5
Bernhard Efler <b.efler@kiwi-suite.com>
2017-11-07 14:01:27 +0100

    CS fixes

--- src/Stdlib/Filesystem.php
+++ src/Stdlib/Filesystem.php
@@ -20,11 +20,12 @@ abstract class Filesystem
      */
     public static function isAbsolutePath($file)
     {
-        return \strspn($file, '/\\', 0, 1)
-        || (\mb_strlen($file) > 3 && \ctype_alpha($file[0])
-            && \mb_substr($file, 1, 1) === ':'
-            && \strspn($file, '/\\', 2, 1)
-        )
-        || null !== \parse_url($file, PHP_URL_SCHEME);
+        return \strspn($file, '/\\', 0, 1) ||
+            (
+                \mb_strlen($file) > 3 && \ctype_alpha($file[0]) &&
+                \mb_substr($file, 1, 1) === ':' &&
+                \strspn($file, '/\\', 2, 1)
+            ) ||
+            null !== \parse_url($file, PHP_URL_SCHEME);
     }
 }
