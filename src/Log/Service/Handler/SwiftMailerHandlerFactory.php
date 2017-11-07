612171b72f8148a1d7584619a8660e643ce00428
Bernhard Efler <b.efler@kiwi-suite.com>
2017-11-07 11:07:40 +0100

    Fix SwiftMailer Logger

--- src/Log/Service/Handler/SwiftMailerHandlerFactory.php
+++ src/Log/Service/Handler/SwiftMailerHandlerFactory.php
@@ -10,10 +10,11 @@
  * @author kiwi suite <dev@kiwi-suite.com>
  */
 
-
 namespace Core42\Log\Service\Handler;
 
 use Interop\Container\ContainerInterface;
+use Monolog\Handler\BufferHandler;
+use Monolog\Handler\FingersCrossedHandler;
 use Monolog\Handler\SwiftMailerHandler;
 use Monolog\Logger;
 use Zend\ServiceManager\Exception\ServiceNotCreatedException;
@@ -25,20 +26,43 @@ class SwiftMailerHandlerFactory implements FactoryInterface
      * @param ContainerInterface $container
      * @param string $requestedName
      * @param array|null $options
-     * @return SwiftMailerHandler
+     * @return SwiftMailerHandler|BufferHandler|FingersCrossedHandler
      */
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        if (empty($options['mailer'])) {
-            throw new ServiceNotCreatedException('mailer option not found for SwiftMailer');
+        if (empty($options['transport'])) {
+            throw new ServiceNotCreatedException('transport option not found for SwiftMailer');
         }
-        if (empty($options['message'])) {
-            throw new ServiceNotCreatedException('message option not found for SwiftMailer');
+        if (empty($options['to'])) {
+            throw new ServiceNotCreatedException('to option not found for SwiftMailer');
+        }
+
+        $transport = $container->get($options['transport']);
+        $mailer = \Swift_Mailer::newInstance($transport);
+
+        $message = \Swift_Message::newInstance();
+        if (\is_array($options['to'])) {
+            foreach ($options['to'] as $to) {
+                $message->addTo($to);
+            }
+        } else {
+            $message->addTo($options['to']);
+        }
+        if (!empty($options['from'])) {
+            $message->addFrom($options['from']);
+        }
+        if (!empty($options['subject'])) {
+            $message->setSubject($options['subject']);
         }
 
         $level = (!empty($options['level'])) ? $options['level'] : Logger::ERROR;
         $bubble = (!empty($options['bubble'])) ? $options['bubble'] : true;
 
-        return new SwiftMailerHandler($options['mailer'], $options['message'], $level, $bubble);
+        $handler = new SwiftMailerHandler($mailer, $message, $level, $bubble);
+        if (isset($options['buffer']) && $options['buffer'] === true) {
+            return new FingersCrossedHandler($handler, null, 0, true, false);
+        }
+
+        return $handler;
     }
 }
